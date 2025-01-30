800bac11edc29c87347c1ede888978a796d3e1b6
tvsrr <tvsrr.1996@gmail.com>
2025-01-30 01:31:13 -0800

    ball chaser completed

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -6,5 +6,4 @@
 # testing this directory and lists subdirectories to be tested as well.
 subdirs(gtest)
 subdirs(my_robot)
-subdirs(simple_arm)
 subdirs(ball_chaser)
--- build/ball_chaser/catkin_generated/installspace/ball_chaserConfig.cmake
+++ build/ball_chaser/catkin_generated/installspace/ball_chaserConfig.cmake
@@ -91,9 +91,9 @@ endif()
 # flag project as catkin-based to distinguish if a find_package()-ed project is a catkin project
 set(ball_chaser_FOUND_CATKIN_PROJECT TRUE)
 
-if(NOT " " STREQUAL " ")
+if(NOT "include " STREQUAL " ")
   set(ball_chaser_INCLUDE_DIRS "")
-  set(_include_dirs "")
+  set(_include_dirs "include")
   foreach(idir ${_include_dirs})
     if(IS_ABSOLUTE ${idir} AND IS_DIRECTORY ${idir})
       set(include ${idir})
@@ -145,7 +145,7 @@ foreach(library ${libraries})
   endif()
 endforeach()
 
-set(ball_chaser_EXPORTED_TARGETS "")
+set(ball_chaser_EXPORTED_TARGETS "ball_chaser_generate_messages_cpp;ball_chaser_generate_messages_eus;ball_chaser_generate_messages_lisp;ball_chaser_generate_messages_nodejs;ball_chaser_generate_messages_py")
 # create dummy targets for exported code generation targets to make life of users easier
 foreach(t ${ball_chaser_EXPORTED_TARGETS})
   if(NOT TARGET ${t})
@@ -153,7 +153,7 @@ foreach(t ${ball_chaser_EXPORTED_TARGETS})
   endif()
 endforeach()
 
-set(depends "")
+set(depends "message_generation;roscpp;std_msgs;geometry_msgs")
 foreach(depend ${depends})
   string(REPLACE " " ";" depend_list ${depend})
   # the package name of the dependency must be kept in a unique variable so that it is not overwritten in recursive calls
@@ -182,7 +182,7 @@ foreach(depend ${depends})
   list(APPEND ball_chaser_EXPORTED_TARGETS ${${ball_chaser_dep}_EXPORTED_TARGETS})
 endforeach()
 
-set(pkg_cfg_extras "")
+set(pkg_cfg_extras "ball_chaser-msg-extras.cmake")
 foreach(extra ${pkg_cfg_extras})
   if(NOT IS_ABSOLUTE ${extra})
     set(extra ${ball_chaser_DIR}/${extra})
