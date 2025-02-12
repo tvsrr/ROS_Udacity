24c5afc321de6f5d7554df17984c5254a5e1548a
tvsrr <tvsrr.1996@gmail.com>
2025-02-12 00:01:05 -0800

    pgm installation

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -6,5 +6,9 @@
 # testing this directory and lists subdirectories to be tested as well.
 subdirs(gtest)
 subdirs(my_robot)
-subdirs(simple_arm)
+subdirs(turtlebot_simulator/turtlebot_gazebo)
+subdirs(turtlebot_simulator/turtlebot_simulator)
+subdirs(turtlebot_simulator/turtlebot_stage)
+subdirs(turtlebot_simulator/turtlebot_stdr)
 subdirs(ball_chaser)
+subdirs(pgm_map_creator)
